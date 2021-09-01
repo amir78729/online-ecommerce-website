f28cbc405acea9336de33ce82a62ea03bcf570ba
amir78729 <amir78729@gmail.com>
2021-09-01 12:13:51 +0430

    adding items and quantity to local storage

--- scripts/productInfo.js
+++ scripts/productInfo.js
@@ -1,4 +1,5 @@
 let urlParams = new URLSearchParams(window.location.search);
+let product;
 
 const createProductElement = (product) => {
     document.getElementById('product-title').textContent = product.title;
@@ -34,7 +35,7 @@ const getProductInfo = async () => {
         console.log('getting product info...');
         let productId = urlParams.get('productId');
 
-        const product = await fetch('https://fakestoreapi.com/products/' + productId)
+        product = await fetch('https://fakestoreapi.com/products/' + productId)
             .then(res=>res.json());
         console.log(product);
         createProductElement(product);
@@ -42,6 +43,11 @@ const getProductInfo = async () => {
         console.log(err);
         console.log('failed to load product data')
     }
+}
+
+const addToCart = () => {
+    let quantity = document.getElementById('product-quantity').value;
+    localStorage.setItem(JSON.stringify(product),quantity);
 }
 
 getProductInfo();

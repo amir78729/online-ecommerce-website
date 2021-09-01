0699cbafd4ccabd6fa864e9c218176d744720070
amir78729 <amir78729@gmail.com>
2021-09-01 10:55:28 +0430

    cart page created

--- scripts/navbarCategories.js
+++ scripts/navbarCategories.js
@@ -1,5 +1,10 @@
+
 const getCategories = async () => {
     try {
+
+        let thisCategory = urlParams.get('category');
+
+
         let categories = await fetch('https://fakestoreapi.com/products/categories')
             .then(res=>res.json());
 
@@ -10,6 +15,10 @@ const getCategories = async () => {
         let navAHome = document.createElement('a');
         let ATextHome = document.createTextNode('Home');
 
+        if (thisCategory === 'all' || thisCategory === null) {
+            navAHome.style.color = '#DAA520';
+        }
+
         navAHome.appendChild(ATextHome);
         navAHome.setAttribute('href','home.html')
         navLiHome.appendChild(navAHome);
@@ -19,7 +28,15 @@ const getCategories = async () => {
             let navLi = document.createElement('li');
             let navA = document.createElement('a');
             let AText = document.createTextNode(categories[i]);
-            nav.style.color = '#eee';
+
+            if (thisCategory === categories[i]) {
+                navA.style.color = 'goldenrod';
+                console.log(thisCategory === categories[i])
+            } else {
+                navA.style.color = '#eee';
+
+            }
+
 
             navA.appendChild(AText);
             navA.setAttribute('href','home.html?category=' + categories[i])
